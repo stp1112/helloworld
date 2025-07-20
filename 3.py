2ad10a38799690b8f8aac544b051a4d1a52aa3c6
stp1112 <stpgames13@gmail.com>
2025-07-20 15:40:35 +0300

    add 2 files

---
+++ 3.py
@@ -0,0 +1,4 @@
+name = input("Як тебе звати? ").strip().title()
+age = input("Скількі тобі років? ").strip()
+sport = input("яке твое хобі? ").strip()
+print(f"Привіт, мене звати {name}, мені {age} років, я люблю {sport}.")
